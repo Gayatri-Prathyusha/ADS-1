c0fbd8939864b1178b6dfd18fdc2a1abec067621
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-09-29 12:32:07 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- Assignments/Module 6/Assignment-1/Solution.java
+++ Assignments/Module 6/Assignment-1/Solution.java
@@ -1,14 +1,32 @@
 import java.util.Scanner;
+/**
+ * Main class
+ */
 public class Solution {
-    Solution() {
+    /**
+     * class
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private Solution() {
+        /**
+         * Empty constructor to avoid checkstyle.
+         */
 
     }
+    /**
+     * Main function.
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  { exception_description }
+     */
     public static void main(String[] args)throws Exception {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String p = sc.nextLine();
         String q = sc.nextLine();
-        switch(input){
+        switch(input) {
             case "numberToDigits":
                 LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
                 LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
