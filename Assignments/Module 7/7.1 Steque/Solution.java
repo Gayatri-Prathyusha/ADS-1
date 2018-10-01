0e0bd506f6278673e1e8e0dbf5a6aee50716684c
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-10-01 12:33:58 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- Assignments/Module 7/7.1 Steque/Solution.java
+++ Assignments/Module 7/7.1 Steque/Solution.java
@@ -1,35 +1,40 @@
 import java.util.Scanner;
-class Solution {
-	Solution() {
+public class Solution {
+    /**
+     * Empty Constructor for checkstyle.
+     */
+    private Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc =  new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		if(n > 0) {
-			for(int i = 0; i < n; i++) {
-				Steque steak = new Steque();				
-		    
-		    while (sc.hasNext()) {
-		    	String[] tokens = sc.nextLine().split(" ");
-		    	switch (tokens[0]) {
-		    		case "push":
-		    		steak.push(Integer.parseInt(tokens[1]));
-		    		break;
-		    		case "pop":
-		    		steak.pop();
-		    		break;
-		    		case "enqueue":
-		    		steak.enqueue(Integer.parseInt(tokens[1]));
-		    		break;
-		    		default:
-		    		break;
-		    	}
-		    }
-		    }
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                Steque steak = new Steque();
+            while (sc.hasNext()) {
+                String[] tokens = sc.nextLine().split(" ");
+                switch (tokens[0]) {
+                    case "push":
+                    steak.push(Integer.parseInt(tokens[1]));
+                    break;
+                    case "pop":
+                    steak.pop();
+                    break;
+                    case "enqueue":
+                    steak.enqueue(Integer.parseInt(tokens[1]));
+                    break;
+                    default:
+                    break;
+                    }
+                }
+            }
+        }
 
-
-	    }
-
-	}
+    }
 }
