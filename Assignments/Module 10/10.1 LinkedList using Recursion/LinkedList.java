8294631f45b3b2862eafa952df2a601462b600cb
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-10-04 17:37:12 +0530

    "ads1_m10_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/Module 10/10.1 LinkedList using Recursion/LinkedList.java
@@ -0,0 +1,81 @@
+class LinkedList {
+
+	private class Node {
+
+		int data;
+		Node next;
+
+		Node() {}
+
+		Node(int data) {
+			this(data, null);
+		}
+
+		Node(int data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
+	public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node node) {
+        next = node;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setdata(int elem) {
+        data = elem;
+    }
+
+    public String toString() {
+        return data + "";
+    }
+	}
+	private Node head;
+	private int size = 0;
+
+	public Node insertAt(int data, int position) {
+    if (position == 0) {
+        Node node = new Node();
+        node.data = data;
+        node.next = head;
+        return node;
+    }
+    head.next = insertAt(data, position - 1);
+    return head;
+
+	}
+	public String[] reverse() {
+		return null;
+
+	}		
+
+	public boolean isEmpty1() {
+		return head == null;
+	}
+
+
+	/**
+	 * 
+	 * Time complexity : N.
+	 * n is the size of list.
+	 *
+	 * @param      e     { parameter_description }
+	 */
+	public String toString() {
+        String result = "";
+        result = result + head.getData();
+        Node temp = head.getNext();
+        while (temp != null) {
+            result = result + ", " + temp.getData();
+            temp = temp.getNext();
+            }
+        return result;
+        
+    }
+
+}
---
+++ Assignments/Module 10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		LinkedList lst = new LinkedList();
+		String[] tokens = sc.nextLine().split(" ");
+		switch (tokens[0]){
+		case "insertAt" :
+		lst.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+		break;
+		case "reverse":
+		lst.reverse();
+		break;
+		default:
+		break;
+
+	    }
+    }
+
+}
