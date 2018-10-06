737f7183a58b18306bd0827087da6a5410c793f8
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-10-06 13:34:29 +0530

    "ads1_m7_a1: 2/2 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 7/7.1 Steque/LinkedListGeneric.java
+++ Assignments/Module 7/7.1 Steque/LinkedListGeneric.java
@@ -1,36 +1,83 @@
+/**
+ * Class for linked list generic.
+ *
+ * @param      <E>   { parameter_description }
+ */
 class LinkedListGeneric<E> {
 
+	/**
+	 * Class for node.
+	 */
 	private class Node {
 
 		E data;
 		Node next;
 
+		/**
+		 * Constructs the object.
+		 */
 		Node() {}
 
+		/**
+		 * Constructs the object.
+		 *
+		 * @param      data  The data
+		 */
 		Node(E data) {
 			this(data, null);
 		}
 
+		/**
+		 * Constructs the object.
+		 *
+		 * @param      data  The data
+		 * @param      next  The next
+		 */
 		Node(E data, Node next) {
 			this.data = data;
 			this.next = next;
 		}
+	/**
+	 * Gets the next.
+	 *
+	 * @return     The next.
+	 */
 	public Node getNext() {
         return next;
     }
 
+    /**
+     * Sets the next.
+     *
+     * @param      node  The node
+     */
     public void setNext(Node node) {
         next = node;
     }
 
+    /**
+     * Gets the data.
+     *
+     * @return     The data.
+     */
     public E getData() {
         return data;
     }
 
+    /**
+     * Sets the element which is sent as a parameter to data variable.
+     *
+     * @param      elem  The element
+     */
     public void setdata(E elem) {
         data = elem;
     }
 
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return data + "";
     }
@@ -39,6 +86,11 @@ class LinkedListGeneric<E> {
 	private Node tail;
 	private int size = 0;
 
+	/**
+	 * Adds a 1.
+	 *
+	 * @param      e     {sends the values of the object type}.
+	 */
 	public void add1(E e) {
 		Node node = new Node();
 		node.data = e;
@@ -52,6 +104,11 @@ class LinkedListGeneric<E> {
 		head = node;
 		size++;
 	}		
+	/**
+	 * adds the object type variable to the last of the list.
+	 *
+	 * @param      e     { parameter_description }
+	 */
 	public void addlast(E e) {
 		if(head == null && tail == null) {
 			Node node = new Node();
@@ -69,6 +126,9 @@ class LinkedListGeneric<E> {
 
 }
 
+	/**
+	 * removes the first node.
+	 */
 	public void remove() {
 		if(size == 1){
 			tail = head = null;
@@ -82,10 +142,22 @@ class LinkedListGeneric<E> {
 		size--;
 }
 
+	/**
+	 * Determines if empty 1.
+	 *
+	 * @return     True if empty 1, False otherwise.
+	 */
 	public boolean isEmpty1() {
 		return head == null;
 	}
 
+	/**
+	 * 
+	 * Time complexity : N.
+	 * n is the size of list.
+	 *
+	 * @param      e     { parameter_description }
+	 */
 	public String toString() {
         String result = "";
         if (head == null) {
