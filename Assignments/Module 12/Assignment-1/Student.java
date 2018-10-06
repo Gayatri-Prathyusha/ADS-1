9a7fdc9481e3c2cf6d32adf43d55c257db2bcd88
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-10-06 13:04:14 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 12/Assignment-1/Student.java
+++ Assignments/Module 12/Assignment-1/Student.java
@@ -1,15 +1,32 @@
+/**
+ * importing the Date class.
+ */
 import java.util.Date;
+/**
+ * Class for student.
+ */
 class Student {
     String studentname;
-    Date date;
+    String date;
     int subject1;
     int subject2;
     int subject3;
     int totalmarks;
     String reservationcategory;
+    /**
+     * Constructs the object.
+     */
     Student() {
+        /**
+         * Empty Constructor.
+         */
 
     }
+    /**
+     * Constructs the object.
+     *
+     * @param      input  The input
+     */
     Student(final String input) {
         String[] inp = input.split(",");
         studentname = inp[0];
@@ -21,7 +38,18 @@ class Student {
         reservationcategory = inp[6];
     }
 
-    Student(String studentname1, Date date1, int sub1,
+    /**
+     * Constructs the object.
+     *
+     * @param      studentname1          The studentname 1
+     * @param      date1                 The date 1
+     * @param      sub1                  The sub 1
+     * @param      sub2                  The sub 2
+     * @param      sub3                  The sub 3
+     * @param      totalmarks1           The totalmarks 1
+     * @param      reservationcategory1  The reservationcategory 1
+     */
+    Student(String studentname1, String date1, int sub1,
      int sub2, int sub3, int totalmarks1,
       String reservationcategory1) {
        this.studentname = studentname;
@@ -32,34 +60,77 @@ class Student {
        this.totalmarks = totalmarks1;
        this.reservationcategory = reservationcategory1;
     }
+    /**
+     * Gets the student name.
+     *
+     * @return     The student name.
+     */
     public String getStudentName(){
         return studentname;
     }
 
-    public Date getDate() {
+    /**
+     * Gets the date.
+     *
+     * @return     The date.
+     */
+    public String getDate() {
         return date;
     }
 
+    /**
+     * Gets the subject 1.
+     *
+     * @return     The subject 1.
+     */
     public int getSubject1() {
         return subject1;
     } 
 
+    /**
+     * Gets the subject 2.
+     *
+     * @return     The subject 2.
+     */
     public int getSubject2() {
         return subject2;
     }
-     public int getSubject3() {
+    /**
+     * Gets the subject 3.
+     *
+     * @return     The subject 3.
+     */
+    public int getSubject3() {
         return subject3;
      }
-     public int getTotalMarks() {
+    /**
+     * Gets the total marks.
+     *
+     * @return     The total marks.
+     */
+    public int getTotalMarks() {
         return totalmarks;
      }
-     public String getReservationCategory() {
+    /**
+     * Gets the reservation category.
+     *
+     * @return     The reservation category.
+     */
+    public String getReservationCategory() {
         return reservationcategory;
      }
-     
 
 
-     public int compareTo (Student other) {
+
+    /**
+     * compares the comparable Student objects.
+     * Time Complexity : 1
+     *
+     * @param      other  The other
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo (Student other) {
         if (this.totalmarks > other.totalmarks) {
             return 1;
         }
@@ -78,14 +149,19 @@ class Student {
         if (this.subject2 < other.subject2) {
              return -1;
         }
-        // if (this.date > other.date) {
+        // if ((int)this.date > (int) other.date) {
         //    return 1;
   
         // }
         return 0;
     }
 
-     public String toString() {
+    /**
+     * Returns a string representation of the object.
+     * Time Complexity 1
+     * @return     String representation of the object.
+     */
+    public String toString() {
         String str = "";
         return str + getStudentName() + " " + getTotalMarks()
             + " " + getReservationCategory();
