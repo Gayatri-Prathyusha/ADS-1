c1040eb0310498e45585bd87dc1fa986c25f795b
Gayatri-Prathyusha <gayatri121995@msitprogram.net>
2018-10-13 13:06:42 +0530

    "ads1_week3_exam: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 18/Week-3_Assignment-1/Solution.java
+++ Assignments/Module 18/Week-3_Assignment-1/Solution.java
@@ -43,37 +43,87 @@ public final class Solution {
 		Scanner scan = new Scanner(System.in);
 
 		int n = Integer.parseInt(scan.nextLine());
+		ArrayList<String> stocks = new ArrayList<String>();
 		ArrayList<Stock> top5 = new ArrayList<Stock>();
 		ArrayList<Stock> least5 = new ArrayList<Stock>();
 		for(int i = 0; i < 6 ; i++) {
 		EveryHour(top5, least5, n, scan);
 	    }
 
-	    // if (scan.hasNext()) {
+	    ArrayList<String> topperformers = new ArrayList<String>();
+	    ArrayList<String> leastperformers = new ArrayList<String>();
 
+	    for (int i = 0; i < top5.size(); i++) {
+	      topperformers.add(top5.get(i).getCompanyName());
+	      // System.out.println(topperformers.get(i));
+	    }
 
-	    // int m = Integer.parseInt(scan.nextLine());
-	    // String[] arr1 = scan.nextLine().split(",");
-	    // BinarySearchST<String, Integer> bst = new BinarySearchST<String, Integer>();
-	    // for(int i = 0; i < m - 1; i++) {
-	    // 	bst.put(arr1[i], i);
-	    // }
-	    // while (scan.hasNext()) {
-            
-     //        switch (arr1[0]) {
-     //            case "get":
-     //            System.out.println(arr1[2]);
-     //            break;
-     //            case "intersection":
+	    for (int i = 0; i < least5.size(); i++) {
+	      leastperformers.add(least5.get(i).getCompanyName());
+	    }
 
+	    BinarySearchST<String, Integer> maxST = new BinarySearchST<String, Integer>();
+	    BinarySearchST<String, Integer> minST = new BinarySearchST<String, Integer>();
 
-                
-     //            break;
-     //            default:
-     //            break;
-     //        }
-     //    }
+	    for (int i = 0; i < stocks.size(); i++) {
+	      String temp = stocks.get(i);
+	      if (!topperformers.contains(temp)) {
+	        maxST.put(temp, 0);
+	      }
+	    }
 
+	    // System.out.println(maxST);
+
+	    for (int i = 0; i < topperformers.size(); i++) {
+	      String temp = topperformers.get(i);
+	      if (!maxST.contains(temp)) {
+	        maxST.put(temp, 1);
+	      } else {
+	        maxST.put(temp, maxST.get(temp) + 1);
+	      }
+	    }
+	    for (int i = 0; i < stocks.size(); i++) {
+	      String temp = stocks.get(i);
+	      if (!minST.contains(temp)
+	          && !leastperformers.contains(temp)) {
+	        minST.put(temp, 0);
+	      }
+	    }
+
+	    for (int i = 0; i < leastperformers.size(); i++) {
+	      String temp = leastperformers.get(i);
+	      if (!minST.contains(temp)) {
+	        minST.put(temp, 1);
+	      } else {
+	        minST.put(temp, minST.get(temp) + 1);
+	      }
+	    }
+	    int numInp = Integer.parseInt(scan.nextLine());
+
+	    for (int j = 0; j < numInp; j++) {
+	      String[] inp = scan.nextLine().split(",");
+	      switch (inp[0]) {
+	      case "get":
+	        if (inp[1].equals("minST")) {
+	          if (minST.contains(inp[2])) {
+	            System.out.println(minST.get(inp[2]));
+	          } else {
+	            System.out.println(0);
+	          }
+	          break;
+	        } else if (inp[1].equals("maxST")) {
+	          if (maxST.contains(inp[2])) {
+	            System.out.println(maxST.get(inp[2]));
+	          } else {
+	            System.out.println(0);
+	          }
+	          break;
+	        }
+	      case "intersection":
+	        break;
+	      default:
+	        break;
+	      }
+	    }
+	  }
 	}
-
-}
